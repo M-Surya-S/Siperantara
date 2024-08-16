973d5cc71b7255aab7cc2b17470e2ef7511248ca
DapaScript <daffa9723@gmail.com>
2024-08-16 22:11:50 +0800

    update guide buyer ama seller

--- resources/views/detail/detail-buyers.blade.php
+++ resources/views/detail/detail-buyers.blade.php
@@ -1,24 +1,104 @@
-@extends("layouts.app")
-@section("content")
-<section class="bd-breadcrumb-area p-relative fix">
-    <!-- breadcrumb background image -->
-    <div class="bd-breadcrumb-bg" data-background="assets/images/breadcrumb/breadcrumb-thumb-01.png"></div>
-    <div class="bd-breadcrumb-wrapper p-relative">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-10">
-                    <div class="bd-breadcrumb">
-                        <div class="bd-breadcrumb-content text-center">
-                            <h1 class="bd-breadcrumb-title">Buyer's Guide</h1>
-                            <div class="bd-breadcrumb-list">
-                                <span><a href="/"><i class="icon-home"></i>Home</a></span>
-                                <span>Buyer's Guide</span>
+@extends('layouts.app')
+@section('content')
+<main>
+    <!-- Breadcrumb area start -->
+    <section class="bd-breadcrumb-area p-relative fix">
+        <!-- breadcrumb background image -->
+        <div class="bd-breadcrumb-bg" data-background="assets/images/breadcrumb/breadcrumb-thumb-01.png"></div>
+        <div class="bd-breadcrumb-wrapper p-relative">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-10">
+                        <div class="bd-breadcrumb">
+                            <div class="bd-breadcrumb-content text-center">
+                                <h1 class="bd-breadcrumb-title">Guide</h1>
+                                <div class="bd-breadcrumb-list">
+                                    <span><a href="/"><i class="icon-home"></i>Home</a></span>
+                                    <span>Buyer's Guide</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+    <!-- Breadcrumb area end -->
+
+    <section class="bd-services-item section-space theme-bg-primary">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xxl-6 col-xl-6 col-lg-6">
+                    <div class="section-title-wrapper anim-wrapper section-title-space text-center animation-style-3">
+                        <h2 class="section-title title-animation">Buyer's Guide</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row g-5 justify-content-between justify-content-md-center">
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="/buyers-guide">1. Lorem Ipsum</a></h3>
+                            <p class="description">The rent property kitchen invites you to whip up delicious meals, while the cozy bedroom promises sleep</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".3s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="#">2. Lorem Ipsum</a></h3>
+                            <p class="description">Choose us for your real estate needs and experience unparalleled expertise and dedication.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".5s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="#">3. Lorem Ipsum</a></h3>
+                            <p class="description">With years of industry experience, our team boasts an in-depth understanding of the market knowledge.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="#">4. Lorem Ipsum</a></h3>
+                            <p class="description">The rent property kitchen invites you to whip up delicious meals, while the cozy bedroom promises sleep</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="#">5. Lorem Ipsum</a></h3>
+                            <p class="description">The rent property kitchen invites you to whip up delicious meals, while the cozy bedroom promises sleep</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6">
+                    <div class="services-item style-one wow bdFadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                        <div class="content">
+                            <h3 class="title"><a href="#">6. Lorem Ipsum</a></h3>
+                            <p class="description">The rent property kitchen invites you to whip up delicious meals, while the cozy bedroom promises sleep</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Centered footer button -->
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="footer-btn-wrap">
+                        <a class="bd-btn btn-style btn-hover-x hover-white" href="#"><i class="fa-regular fa-arrow-right-long"></i>Download Our Guide</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
 @endsection
