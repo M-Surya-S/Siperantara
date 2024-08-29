3390ef1d2722cd2b0cf96d289c465818294a0cb5
M-Surya-S <muhammadsurya216@gmail.com>
2024-08-29 10:04:05 +0800

    download seller guide kelar

--- app/Http/Controllers/GuideController.php
+++ app/Http/Controllers/GuideController.php
@@ -19,10 +19,22 @@ class GuideController extends Controller
         return view('guest.guide.buyers.detail-buyers-guide', compact('title'));
     }
     
+    public function seller()
+    {
+        $title = 'Seller Guide';
+        return view('guest.guide.seller.detail-seller-guide', compact('title'));
+    }
+    
     public function buyersIndex()
     {
         $title = 'Buyers Guide';
         return view('guest.guide.buyers.buyers-guide-download', compact('title'));
+    }
+    
+    public function sellerIndex()
+    {
+        $title = 'Seller Guide';
+        return view('guest.guide.seller.seller-guide-download', compact('title'));
     }
 
     public function buyersDownload(Request $request)
@@ -35,6 +47,24 @@ class GuideController extends Controller
         ]);
 
         $filePath = public_path('assets/guide/Siperantara Buyers Guide.pdf'); 
+
+        if (!file_exists($filePath)) {
+            return back()->withErrors(['file' => 'File tidak ditemukan.']);
+        }
+
+        return response()->download($filePath);
+    }
+    
+    public function sellerDownload(Request $request)
+    {
+        DownloadGuide::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'information' => 'Download Seller Guide',
+        ]);
+
+        $filePath = public_path('assets/guide/Siperantara Seller Guide.pdf'); 
 
         if (!file_exists($filePath)) {
             return back()->withErrors(['file' => 'File tidak ditemukan.']);
--- routes/web.php
+++ routes/web.php
@@ -94,14 +94,10 @@ Route::get('/guide', [GuideController::class, 'index']);
 Route::get('/guide/buyers', [GuideController::class, 'buyers']);
 Route::get('/guide/buyers/download', [GuideController::class, 'buyersIndex']);
 Route::post('/guide/buyers/download', [GuideController::class, 'buyersDownload'])->name('buyers-download');
-
 // Seller Guide
-Route::get('/guide/seller', function () {
-    return view('guest.guide.seller.detail-seller-guide', ['title' => 'Seller Guide']);
-});
-Route::get('/guide/seller/download', function () {
-    return view('guest.guide.seller.seller-guide-download', ['title' => 'Seller Guide']);
-});
+Route::get('/guide/seller', [GuideController::class, 'seller']);
+Route::get('/guide/seller/download', [GuideController::class, 'sellerIndex']);
+Route::post('/guide/seller/download', [GuideController::class, 'sellerDownload'])->name('seller-download');
 
 // Blog
 Route::get('/blog', [BlogHomeController::class, 'index']);
