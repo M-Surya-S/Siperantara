eefb928369ea4ce6b4656727fdaed075c991465a
Dapascriptt <serahbapakmu@gmail.com>
2024-10-28 14:43:19 +0800

    update button

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -175,6 +175,18 @@
                                                     city life.</p>
                                             </div>
                                         </div>
+                                        <a href="#" class="bd-animated-btn style-two mt-20">
+                                            <svg viewBox="0 0 24 24" class="arrow-right" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+                                                </path>
+                                            </svg>
+                                            <span class="bd-animated-btn-text">Explore More</span>
+                                            <span class="bd-animated-btn-circle"></span>
+                                            <svg viewBox="0 0 24 24" class="arrow-left" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+                                                </path>
+                                            </svg>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
